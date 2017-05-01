161110a9f27fb86a3e91be953d3713c0afdc2806
roberth26 <robbiehall26@gmail.com>
2017-04-30 23:21:16 -0700

    parser cleanup/comments

--- src/utils/Parser.tsx
+++ src/utils/Parser.tsx
@@ -5,108 +5,7 @@ import FormContainer from '../containers/FormContainer/FormContainer';
 import Paragraph from '../components/primitives/Paragraph';
 
 const Parser = new HtmlToReact.Parser();
-
-/*
-interface IParserProps {
-    children?: React.ReactChildren;
-}
-
-class Parser extends React.Component<IParserProps, {}> {
-    parseShortcode = ( str: string ): React.ReactElement<any> => {
-        const type = str.split( ' ' )[ 0 ].substring( 1 );
-        const shortcode = str.match( /[\w-]+="[^"]*"/g );
-        if ( !shortcode ) {
-            return <span>{str}</span>;
-        }
-
-        const params = new Map<string, string>(
-            shortcode.map(( param: string ) => {
-                const key = param.split( '=' )[ 0 ];
-                const value = param.split( '"' )[ 1 ];
-
-                return [ key, value ] as [ string, string ];
-            })
-        );
-
-        switch ( type ) {
-            case 'form': {
-                const id = Number.parseInt( params.get( 'id' ) );
-                return <FormContainer formId={id} />;
-            }
-            default: {
-                return <span>{str}</span>;
-            }
-        }
-    }
-
-    renderShortcode = ( element: React.ReactElement<any> ): React.ReactElement<any> => {
-        return React.cloneElement( element, {
-            children: React.Children.map( element.props.children, ( child: React.ReactChild ) => {
-                if ( !React.isValidElement( child ) ) {
-                    return ( child as string ).replace( /[“”‘’″]/g, '"' )
-                        .split( /(\[.*?\])/ )
-                        .map(( str: string ) => {
-                            if ( str && str.match( /(\[.*?\])/ ) ) {
-                                return this.parseShortcode( str );
-                            }
-
-                            return <span>{str}</span>;
-                        });
-                }
-                return child;
-            })
-        });
-    }
-
-    renderShortcodes = ( nodes: React.ReactChildren ) => {
-        return React.Children.map( nodes, ( node: React.ReactChild ) => {
-            if ( !React.isValidElement( node ) ) {
-                return node;
-            }
-
-            node = node as React.ReactElement<any>;
-
-            // prevent invalid html...
-            if ( node.type === 'p' ) {
-                node = <Paragraph {...node.props} />;
-            }
-
-            if ( node.props.children ) {
-                node = React.cloneElement( node, {
-                    children: this.renderShortcodes( node.props.children ) // recursive call
-                });
-            }
-
-            return this.renderShortcode( node );
-        });
-    }
-
-    render() {
-        const { children } = this.props;
-
-        return (
-            <span>
-                {this.renderShortcodes( children )}
-            </span>
-        );
-    }
-}
-
-export default function parse( str: string ) {
-    if ( !str ) {
-        return null;
-    }
-
-    const nodes = HtmlToReact.parse(
-        DOMPurify.sanitize( str.trim() )
-    );
-
-    return <Parser>{nodes}</Parser>;
-};
-*/
-
 const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions( React );
-
 const processingInstructions = [
     {
         shouldProcessNode: node => node.name === 'form',
@@ -122,6 +21,7 @@ const processingInstructions = [
             const parsed = processNodeDefinitions.processDefaultNode( node, children, index );
 
             if ( node.name === 'p' ) {
+                // TODO: don't replace ALL p's, only ones containing invalid html
                 return React.createElement( Paragraph, { ...parsed.props, key: index }, children );
             }
 
