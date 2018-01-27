a23a3e18a52d765d07e1edcba8cf155fe09ce7dc
roberth26 <robbiehall26@gmail.com>
2018-01-27 15:43:34 -0800

    fixed nav on mobile should be vertical

--- src/components/Menu/primitives/List.tsx
+++ src/components/Menu/primitives/List.tsx
@@ -6,7 +6,7 @@ export default styled.ul`
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
-    ${props => props.vertical ? 'flex-direction: column;' : ''}
+    flex-direction: column;
 
     li {
         margin: ${props => props.vertical ? '16px 0' : '0 16px'};
@@ -15,5 +15,13 @@ export default styled.ul`
     svg {
         height: 24px !important;
         width: auto !important;
+    }
+
+    @media ( min-width: 768px ) {
+        flex-direction: ${props => props.vertical ? 'column' : 'row'};
+
+        li {
+            margin: ${props => props.vertical ? '16px 0' : '0 16px'};
+        }
     }
 `;
